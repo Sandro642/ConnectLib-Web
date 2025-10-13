b72cfbe43a899786eac1e223ec74f66473336722
Sandro Soria <sandro33810@gmail.com>
2025-10-13 08:49:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['connectlib.systemsolutiongroup.xyz'],
+    allowedHosts: ['connectlib.xyz'],
   },
 })
